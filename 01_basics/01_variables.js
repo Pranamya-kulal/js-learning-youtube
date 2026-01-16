a8e559e2cb85ea85acace21f552eda89724b5125
Pranamya-kulal <141472093+Pranamya-kulal@users.noreply.github.com>
2026-01-17 00:03:53 +0530

    Create 01_variables.js
    added const and var code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =144573
+let email="pran@google.com"
+var accPassword ="12345"
+accountCity= "Jaipur"
+let accountstate;
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,email,accPassword,accountCity,accountstate])
