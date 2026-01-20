16a9c0a4c8f3c9723aebcdd1eb4ddab74d80e177
Pranamya-kulal <141472093+Pranamya-kulal@users.noreply.github.com>
2026-01-20 14:06:10 +0530

    added data_types notes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,14 @@
+"use strict"; //treats all js code a newer version 
+//alert(3+3) //we are using node js not browser
+//code readability shud be high
+/*
+number ,bigInt ,string  ,boolean
+null -standalone value (it is also a type)
+undefined -value not defined that is variable is declared but value is not defined
+while null means no value ,that is empty 
+
+symbols also datatype 
+*/
+let age=21
+console.log(typeof null)  // output will be object
+console.log(typeof undefined)  // output will be undefined
